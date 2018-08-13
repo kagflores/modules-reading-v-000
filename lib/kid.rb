295c2d0d0fc0c4fe5850d4820f9bee947c2ea431
Karen Flores <karen.ag.flores@gmail.com>
2018-08-13 15:25:43 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -5,4 +5,11 @@ class Kid
   extend MetaDancing
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 end
